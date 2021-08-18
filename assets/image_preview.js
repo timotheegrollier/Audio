17ddf4fab1b3db7feac21d07d0b4500dddff289d
Timo <timotheegrollier@gmail.com>
2021-08-18 23:21:13 +0200

    fix problem with image name

--- assets/image_preview.js
+++ assets/image_preview.js
@@ -3,17 +3,17 @@ let errorExt = document.getElementById("errorExt");
 
 if (uploadImg) {
   uploadImg.onchange = (evt) => {
-    const [file] = uploadImg.files;
-    let fileName = [file][0].name;
+    const file = uploadImg.files;
     let extensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
 
     if (file) {
+
       // On vérifie l'extension du fichier grace à la regex
-      if (extensions.exec(fileName)) {
+      if (extensions.exec(file[0].name)) {
         errorExt.classList.add("d-none");
         $("#blah").css("display", "block");
         $(".vich-image a").hide();
-        blah.src = URL.createObjectURL(file);
+        blah.src = URL.createObjectURL(file[0]);
       } else {
         $("#blah").css("display", "none");
         errorExt.classList.remove("d-none");
