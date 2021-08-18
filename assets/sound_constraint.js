85aadbc2199f865a2132ba7f4f40d246674b3d44
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-08-18 19:29:02 +0200

    Fix les vérifs coté Front

--- assets/sound_constraint.js
+++ assets/sound_constraint.js
@@ -1,23 +1,35 @@
 let uploadSound = document.querySelector(".form_soundFile_file");
 let errorExtSound = document.getElementById("errorExtSound");
+let errorSizeSound = document.getElementById("errorSizeSound");
 
 if (uploadSound) {
-    uploadSound.onchange = (evt) => {
-        const [file] = uploadSound.files;
-        let fileName = [file][0].name;
-        let extensions = /(\.mp3|\.ogg|\.aac|\.mpeg|\.oga)$/i;
+  uploadSound.onchange = (e) => {
+    const file = uploadSound.files;
+    let extensions = /(\.mp3|\.ogg|\.aac|\.mpeg)$/i;
 
-        if (file) {
-            // On vérifie l'extension du fichier grace à la regex
-            if (extensions.exec(fileName)) {
-                errorExtSound.classList.add("d-none");
+    if (file[0]) {
+      // On vérifie l'extension du fichier grace à la regex
+      if (extensions.exec(file[0].name)) {
+        errorExtSound.classList.add("d-none");
 
-            } else {
-                errorExtSound.classList.remove("d-none");
-                $(".loader").css("display", "none");
-            }
-        }
-    };
+      } else {
+          $('#uploadBtn').attr('disabled','disabled')
+        errorExtSound.classList.remove("d-none");
+      }
+
+    // On vérifie que le fichier ne dépasse pas 100Mo
+      if (file[0].size > 100000000) {
+          $('#uploadBtn').attr('disabled','disabled')
+        errorSizeSound.classList.remove("d-none");
+
+      } else {
+        errorSizeSound.classList.add("d-none");
+      }
+
+    //   On réactive le button uniquement si les deux conditions sont remplie
+      if(extensions.exec(file[0].name) && file[0].size < 100000000){
+        $('#uploadBtn').removeAttr('disabled')
+      }
+    }
+  };
 }
-
-
